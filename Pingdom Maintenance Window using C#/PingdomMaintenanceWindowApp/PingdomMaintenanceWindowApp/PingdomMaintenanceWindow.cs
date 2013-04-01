023059b66cb00b22bf7393c0ed220b66bd72f2d8
Lucifer79 <robert.sindall@gmail.com>
2013-04-01 20:22:57 +0100

    Added C# console app

---
+++ Pingdom Maintenance Window using C#/PingdomMaintenanceWindowApp/PingdomMaintenanceWindowApp/PingdomMaintenanceWindow.cs
@@ -0,0 +1,93 @@
+﻿namespace PingdomMaintenanceWindowApp
+{
+    using System.Configuration;
+
+    using RestSharp;
+
+    public class PingdomMaintenanceWindow
+    {
+        private string AppKey
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["app-key"];
+            }
+        }
+
+        private string Username
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["authorization.username"];
+            }
+        }
+
+        private string Password
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["authorization.password"];
+            }
+        }
+
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+        }
+
+        private const string Host = "https://api.pingdom.com";
+
+        private const string Resource = "/api/2.0/checks";
+
+        private readonly RestClient client;
+
+        private readonly RestRequest request;
+
+        private string message;
+
+        public PingdomMaintenanceWindow()
+        {
+            client = new RestClient(Host);
+            client.AddDefaultHeader("Accept-Encoding" , "gzip, compressed");
+            client.AddDefaultHeader("App-Key", AppKey);
+            client.Authenticator = new HttpBasicAuthenticator(Username, Password);
+
+            request = new RestRequest(Resource, Method.PUT);
+        }
+
+        public void Start()
+        {
+            request.AddParameter("paused", "true");
+            //request.AddBody("paused=true");
+            PerformRequest();
+        }
+
+        public void Stop()
+        {
+            request.AddBody("paused=false");
+            PerformRequest();
+        }
+
+        private void PerformRequest()
+        {
+            var response = client.Execute(request);
+
+            if (response.ResponseStatus == ResponseStatus.Completed)
+            {
+                message = response.Content;
+            }
+            else
+            {
+                message = response.ErrorMessage;
+
+                if (response.ErrorException != null)
+                {
+                    throw response.ErrorException;
+                }
+            }
+        }
+    }
+}
---
+++ Pingdom Maintenance Window using C#/PingdomMaintenanceWindowApp/PingdomMaintenanceWindowApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace PingdomMaintenanceWindowApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Pingdom Maintenance Window options:");
+                Console.WriteLine("");
+                Console.WriteLine("start");
+                Console.WriteLine("stop");
+                Console.WriteLine("");
+                Console.WriteLine("e.g. PingdomMaintenanceWindowApp.exe start");
+                Console.WriteLine("");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            else
+            {
+                string command = args[0];
+
+                var maintenanceWindow = new PingdomMaintenanceWindow();
+
+                if(command == "start")
+                {
+                    Console.WriteLine("Starting maintenance window...");
+                    maintenanceWindow.Start();
+                } 
+                else if (command == "stop")
+                {
+                    Console.WriteLine("Stopping maintenance window...");
+                    maintenanceWindow.Stop();
+                }
+                else
+                {
+                    Console.WriteLine("Unknown command: {0}", command);
+                    return;
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine(maintenanceWindow.Message);
+            }
+        }
+    }
+}
