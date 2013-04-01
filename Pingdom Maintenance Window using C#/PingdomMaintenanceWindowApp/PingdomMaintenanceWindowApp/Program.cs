0ceb344a45fcf6984bad74ec705179d978519bf1
Lucifer79 <robert.sindall@gmail.com>
2013-04-01 21:04:06 +0100

    UX updated

--- Pingdom Maintenance Window using C#/PingdomMaintenanceWindowApp/PingdomMaintenanceWindowApp/Program.cs
+++ Pingdom Maintenance Window using C#/PingdomMaintenanceWindowApp/PingdomMaintenanceWindowApp/Program.cs
@@ -6,6 +6,8 @@ namespace PingdomMaintenanceWindowApp
     {
         static void Main(string[] args)
         {
+            var command = "";
+
             if (args.Length == 0)
             {
                 Console.WriteLine("");
@@ -16,34 +18,35 @@ namespace PingdomMaintenanceWindowApp
                 Console.WriteLine("");
                 Console.WriteLine("e.g. PingdomMaintenanceWindowApp.exe start");
                 Console.WriteLine("");
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey();
+                Console.WriteLine("Or type command now and press enter to proceed...");
+                command = Console.ReadLine();
+            } 
+            else
+            {
+                command = args[0];
+            }
+
+            var maintenanceWindow = new PingdomMaintenanceWindow();
+
+            if(command == "start")
+            {
+                Console.WriteLine("Starting maintenance window...");
+                maintenanceWindow.Start();
+            } 
+            else if (command == "stop")
+            {
+                Console.WriteLine("Stopping maintenance window...");
+                maintenanceWindow.Stop();
             }
             else
             {
-                string command = args[0];
-
-                var maintenanceWindow = new PingdomMaintenanceWindow();
-
-                if(command == "start")
-                {
-                    Console.WriteLine("Starting maintenance window...");
-                    maintenanceWindow.Start();
-                } 
-                else if (command == "stop")
-                {
-                    Console.WriteLine("Stopping maintenance window...");
-                    maintenanceWindow.Stop();
-                }
-                else
-                {
-                    Console.WriteLine("Unknown command: {0}", command);
-                    return;
-                }
-
-                Console.WriteLine("");
-                Console.WriteLine(maintenanceWindow.Message);
+                Console.WriteLine("Unknown command: {0}", command);
+                return;
             }
+
+            Console.WriteLine("");
+            Console.WriteLine(maintenanceWindow.Message);
+            
         }
     }
 }
